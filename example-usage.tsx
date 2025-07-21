0b7fc0ea8be3ba1a5752c77ffdfe7d5c8e3970e7
glatztp <gabrielfellipeglatz@gmail.com>
2025-07-21 00:01:31 -0300

    Remove unused files and update index title
    - Deleted main-BXm-U96U.css and index.html from dist-demo directory.
    - Removed example-usage.tsx file.
    - Updated title in index.html from "gltz-UI - Biblioteca de Componentes" to "gltz - Biblioteca de Componentes".

--- example-usage.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import AdminDashboard from "./src/components/AdminDashboard";
-import "./src/styles/globals.css";
-
-function App() {
-  return <AdminDashboard />;
-}
-
-export default App;
